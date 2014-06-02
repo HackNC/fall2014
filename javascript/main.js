11854f043a28c7d0679f574ef75b481cbb6d7224
Vance Miller <vancemiller@users.noreply.github.com>
2014-06-01 23:18:42 -0400

    Changed header background to use a flickr image

--- javascript/main.js
+++ javascript/main.js
@@ -7,5 +7,31 @@ window.addEventListener("resize", function(event) {
 });
 function setBg() {
 	width = window.innerWidth;
-	document.getElementById("header").style.backgroundImage = "url('./images/purpletint.png'), url('http://placekitten.com/g/" + width + "/520')";	
+	
+	// select image based on drawable area's size
+	var flickrLink;
+	if (width <= 240){
+		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_92a1a2aed3_m.jpg";
+	} else if (width <= 320) {
+		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_92a1a2aed3_n.jpg";
+	} else if (width <= 500) {
+		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_92a1a2aed3.jpg";
+	} else if (width <= 640) {
+		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_92a1a2aed3_z.jpg";
+	} else if (width <= 800) {
+		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_92a1a2aed3_c.jpg";
+	} else if (width <= 1024) {
+		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_92a1a2aed3_b.jpg";
+	} else if (width <= 1600) {
+		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_6a61f5ba84_h.jpg";
+	} else if (width <= 2048) {
+		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_000c0fadc6_k.jpg";
+	} else if (width <= 3051) {
+		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_dc9c9ae6de_o.jpg";	
+	} else {
+		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_6a61f5ba84_h.jpg";
+		// default to something medium sized
+	}
+	
+	document.getElementById("header").style.backgroundImage = "url('./images/purpletint.png'), url("+flickrLink+")";	
 }
