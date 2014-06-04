348c85eec0ed8026fb195213f18af6d35b1f0a15
Vance Miller <vbmiller@cs.unc.edu>
2014-06-04 09:10:07 -0400

    Removed js background loading...added to css

--- javascript/main.js
+++ javascript/main.js
@@ -20,22 +20,22 @@ function setBg() {
 	// } else if (width <= 640) {
 	// 	flickrLink = "https://farm9.staticflickr.com/8230/8431583246_92a1a2aed3_z.jpg";
 	// } else
-	if (width <= 800) {
-		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_92a1a2aed3_c.jpg";
-	} else if (width <= 1024) {
-		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_92a1a2aed3_b.jpg";
-	} else if (width <= 1600) {
-		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_6a61f5ba84_h.jpg";
-	} else if (width <= 2048) {
-		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_000c0fadc6_k.jpg";
-	} else if (width <= 3051) {
-		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_dc9c9ae6de_o.jpg";	
-	} else {
-		flickrLink = "https://farm9.staticflickr.com/8230/8431583246_6a61f5ba84_h.jpg";
-		// default to something medium sized
-	}
+	// if (width <= 800) {
+	// 	flickrLink = "https://farm9.staticflickr.com/8230/8431583246_92a1a2aed3_c.jpg";
+	// } else if (width <= 1024) {
+	// 	flickrLink = "https://farm9.staticflickr.com/8230/8431583246_92a1a2aed3_b.jpg";
+	// } else if (width <= 1600) {
+	// 	flickrLink = "https://farm9.staticflickr.com/8230/8431583246_6a61f5ba84_h.jpg";
+	// } else if (width <= 2048) {
+	// 	flickrLink = "https://farm9.staticflickr.com/8230/8431583246_000c0fadc6_k.jpg";
+	// } else if (width <= 3051) {
+	// 	flickrLink = "https://farm9.staticflickr.com/8230/8431583246_dc9c9ae6de_o.jpg";	
+	// } else {
+	// 	flickrLink = "https://farm9.staticflickr.com/8230/8431583246_6a61f5ba84_h.jpg";
+	// 	// default to something medium sized
+	// }
 	
-	document.getElementById("header").style.backgroundImage = "url('./images/purpletint.png'), url("+flickrLink+")";	
-	document.getElementById("header").style.backgroundSize = "initial, cover";
-	document.getElementById("header").style.backgroundRepeat = "repeat, no-repeat";
+	// document.getElementById("header").style.backgroundImage = "url('./images/purpletint.png'), url("+flickrLink+")";	
+	// document.getElementById("header").style.backgroundSize = "initial, cover";
+	// document.getElementById("header").style.backgroundRepeat = "repeat, no-repeat";
 }
