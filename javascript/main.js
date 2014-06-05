d26633bffed6b2559f7aa963d42b276a666bbe25
jsettlem <jsettlem@cs.unc.edu>
2014-06-05 02:13:14 -0400

    Easter eggs are the best features

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,11 @@
 var width = 1280;
 document.addEventListener("DOMContentLoaded", function(event) {
 	setBg();
+	if (navigator.userAgent.indexOf("Chrome") != -1) {
+		console.log("%c\n _    _               _      _   _   _____ \n| |  | |             | |    | \\ | | / ____|\n| |__| |  __ _   ___ | | __ |  \\| || |     \n|  __  | / _` | / __|| |/ / | . ` || |     \n| |  | || (_| || (__ |   <  | |\\  || |____ \n|_|  |_| \\__,_| \\___||_|\\_\\ |_| \\_| \\_____|\n                                           \n         WELCOME TO HACKER COUNTRY         ", "background-color:black; color:green;font-size:1.5em;");
+	} else {
+		console.log("\n _    _               _      _   _   _____ \n| |  | |             | |    | \\ | | / ____|\n| |__| |  __ _   ___ | | __ |  \\| || |     \n|  __  | / _` | / __|| |/ / | . ` || |     \n| |  | || (_| || (__ |   <  | |\\  || |____ \n|_|  |_| \\__,_| \\___||_|\\_\\ |_| \\_| \\_____|\n                                           \n         WELCOME TO HACKER COUNTRY         ");
+	}
 });
 window.addEventListener("resize", function(event) {
 	setBg();
