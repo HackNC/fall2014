6a3ae6697053be3adbf9fd3c84eb8206d1b6d9ad
Vance Miller <vbmiller@cs.unc.edu>
2014-06-08 10:51:12 -0400

    removed sliding page animation

--- javascript/main.js
+++ javascript/main.js
@@ -1,31 +1,17 @@
-function setHeaderSize() {
-	$(".header").css("height", $(window).height());
-	$(".headerBG").css("height", $(window).height());
-	$(".headerBG::before").css("height", $(window).height());
-}
-
-function initHeaderSize() {
-	$duration = 1400;
-	$(".header").animate({height: $(window).height()}, $duration);
-	$(".headerBG").animate({height: $(window).height()}, $duration);
-	$(".headerBG::before").animate({height: $(window).height()}, $duration);
-}
-
-window.onload=function() {
-	initHeaderSize();
-	if (navigator.userAgent.indexOf("Chrome") != -1) {
-		console.log("%c\n _    _               _      _   _   _____ \n| |  | |             | |    | \\ | | / ____|\n| |__| |  __ _   ___ | | __ |  \\| || |     \n|  __  | / _` | / __|| |/ / | . ` || |     \n| |  | || (_| || (__ |   <  | |\\  || |____ \n|_|  |_| \\__,_| \\___||_|\\_\\ |_| \\_| \\_____|\n                                           \n         WELCOME TO HACKER COUNTRY         ", "background-color:black; color:green;font-size:1.5em;");
-	} else {
-		console.log("\n _    _               _      _   _   _____ \n| |  | |             | |    | \\ | | / ____|\n| |__| |  __ _   ___ | | __ |  \\| || |     \n|  __  | / _` | / __|| |/ / | . ` || |     \n| |  | || (_| || (__ |   <  | |\\  || |____ \n|_|  |_| \\__,_| \\___||_|\\_\\ |_| \\_| \\_____|\n                                           \n         WELCOME TO HACKER COUNTRY         ");
-	}
-}
-
+////////////////// run this after the page has been built //////////////////
+setHeaderSize();
 window.addEventListener("resize", function(event) {
 	setHeaderSize();
 })
 
+////////////////// easter egg //////////////////
+if (navigator.userAgent.indexOf("Chrome") != -1) {
+	console.log("%c\n _    _               _      _   _   _____ \n| |  | |             | |    | \\ | | / ____|\n| |__| |  __ _   ___ | | __ |  \\| || |     \n|  __  | / _` | / __|| |/ / | . ` || |     \n| |  | || (_| || (__ |   <  | |\\  || |____ \n|_|  |_| \\__,_| \\___||_|\\_\\ |_| \\_| \\_____|\n                                           \n         WELCOME TO HACKER COUNTRY         ", "background-color:black; color:green;font-size:1.5em;");
+} else {
+	console.log("\n _    _               _      _   _   _____ \n| |  | |             | |    | \\ | | / ____|\n| |__| |  __ _   ___ | | __ |  \\| || |     \n|  __  | / _` | / __|| |/ / | . ` || |     \n| |  | || (_| || (__ |   <  | |\\  || |____ \n|_|  |_| \\__,_| \\___||_|\\_\\ |_| \\_| \\_____|\n                                           \n         WELCOME TO HACKER COUNTRY         ");
+}
 
-
+////////////////// function definitions //////////////////
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -42,3 +28,8 @@ $(function() {
     }
   });
 });
+function setHeaderSize() {
+	$(".header").css("height", $(window).height());
+	$(".headerBG").css("height", $(window).height());
+	$(".headerBG::before").css("height", $(window).height());
+}
