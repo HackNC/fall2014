92d4219535366435aa26631e82f0c889a54582d7
Vance Miller <vbmiller@cs.unc.edu>
2014-08-03 20:23:57 -0400

    refactored toolbar code, added battery charging menu

--- javascript/toolbar.js
+++ javascript/toolbar.js
@@ -1,14 +1,153 @@
+// function definitions
+
+var fadeTime = 10;
+function openMenu(menu) {
+	menu.fadeIn(fadeTime);
+	menu.css('top', $('.toolbar table').height());
+	if (menu.offset().left + menu.width() > $(window).width()) {
+		menu.css('left', $(window).width()- (menu.offset().left + menu.width()));
+	}
+}
+
+function closeMenu(menu) {
+	menu.fadeOut(fadeTime);
+}
+
+function startTime() {
+	var today = new Date();
+	var month = today.getMonth();
+	var date = today.getDate();
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var amPm = getAmPm(h);
+	switch(month) {
+		case 0:
+		month = 'January';
+		break;
+		case 1:
+		month = 'February';
+		break;
+		case 2:
+		month = 'March';
+		break;
+		case 3:
+		month = 'April';
+		break;
+		case 4:
+		month = 'May';
+		break;
+		case 5:
+		month = 'June';
+		break;
+		case 6:
+		month = 'July';
+		break;
+		case 7:
+		month = 'August';
+		break;
+		case 8:
+		month = 'September';
+		break;
+		case 9:
+		month = 'October';
+		break;
+		case 10:
+		month = 'November';
+		break;
+		case 11:
+		month = 'December';
+		break;
+		default:
+		month = 'Caturday';
+		break;
+
+	}
+	h = fixHours(h);
+	m = fixMinutes(m);
+	$('#clock').html(month + ' ' + date + ', ' + h + ':' + m + ' ' + amPm);
+	var t = setTimeout(function(){
+		startTime()
+	},1000);
+}
+
+function getAmPm(h) {
+	if (h >= 12) {
+		return 'PM';
+	} else {
+		return 'AM';
+	}
+}
+
+function fixHours(h) {
+	if (h > 12) {
+		return h - 12;
+	} else if (h == 0) {
+		return 12;
+	} else {
+		return h;
+	}
+}
+
+function fixMinutes(i) {
+    if (i < 10) {i = '0' + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+var numberOfBatteryImageFrames = 38;
+function getChargeLevel() {
+	var arbitraryDate = new Date(2014, 6, 6, 6, 6, 6, 6);
+	var today = new Date();
+	var HackNC = new Date(2014, 9, 25, 11, 0, 0, 0);
+	
+	var percentCharged;
+
+	if (HackNC < today) {
+		percentCharged = 1;
+	} else {
+		percentCharged = (today - arbitraryDate) / (HackNC - arbitraryDate);
+	}
+	return Math.floor(percentCharged * numberOfBatteryImageFrames);
+}
+
+function doChargeUp() {
+	var maxCharge = getChargeLevel();
+	var imgs = [];
+	for (var i=0; i <= maxCharge; i++) {
+		imgs[i] = new Image();
+		imgs[i].src = './images/toolbar/battery_' + i + '.png';
+	}
+	var i = 0;
+	window.setInterval(function() {
+		if (i <= maxCharge) {
+			$('.toolbar #battery img').attr('src', imgs[i++].src);
+		}
+	}, 250);
+}
+
+function setBatteryMenuText() {
+	var maxCharge = getChargeLevel();
+	$('.toolbar #battery ul.submenu').append('<li>' +
+		Math.floor(maxCharge/numberOfBatteryImageFrames*100) + '% charged</li>');
+}
+
+// code to run
+
 startTime();
+doChargeUp();
+setBatteryMenuText();
+window.setInterval(function() {
+		doChargeUp();
+	}, 10000);
 //display notification center when icon is clicked
 var isNotificationCenterVisible = false;
-$('#notificationCenter').click( function() {
+$('.toolbar #notificationCenter').click( function() {
 	if (isNotificationCenterVisible) {
-		$('.notificationCenter').animate({ right: -250 },'slow', function() {
+		$('.toolbar .notificationCenter').animate({ right: -250 },'slow', function() {
 			$(this).hide();
 		});
 	}
 	else {
-		$('.notificationCenter').css('top', $('.nav table').height());
+		$('.notificationCenter').css('top', $('.toolbar table').height());
 		$('.notificationCenter').show().animate({ right: 0 },'slow');
 
 	}
@@ -19,9 +158,12 @@ $('#notificationCenter').click( function() {
 
 // Menubar clicks
 var lastOpen;
-$('.element-left ul.topnav > li > a').click(function() {
+$('.toolbar ul.topmenu > li > a').click(function() {
 	var button = $(this).parent();
-	var menu = $(this).next('ul.subnav');
+	var menu = $(this).next('ul.submenu');
+	if(!menu.length) {
+		return;
+	}
 	if (lastOpen) {
 		// if a menu is open
 		if (lastOpen.hasClass('open') && button.hasClass('open')) {
@@ -31,7 +173,7 @@ $('.element-left ul.topnav > li > a').click(function() {
 			lastOpen = null;
 		} else {
 			// if this is a different menu than what is already open, close the old and open this
-			closeMenu(lastOpen.find('ul.subnav'));
+			closeMenu(lastOpen.find('ul.submenu'));
 			lastOpen.removeClass('open');
 			openMenu(menu);
 			button.addClass('open');
@@ -47,14 +189,17 @@ $('.element-left ul.topnav > li > a').click(function() {
 });
 
 // Menubar hover.  Hover is enabled after a menu button is clicked.
-$('.element-left ul.topnav > li').hover(
+$('.toolbar ul.topmenu > li').hover(
 	// on mouse enter
 	function() {
 		var button = $(this);
-		var menu = $(this).find('ul.subnav');
+		var menu = $(this).find('ul.submenu');
+		if (!menu.length) {
+			return;
+		}
 		if (lastOpen && !button.hasClass('open')) {
 			// if a menu is open but it's not this menu, open this and close the old
-			closeMenu(lastOpen.find('ul.subnav'));
+			closeMenu(lastOpen.find('ul.submenu'));
 			lastOpen.removeClass('open');
 			openMenu(menu);
 			button.addClass('open');
@@ -65,13 +210,16 @@ $('.element-left ul.topnav > li').hover(
 	function() {
 		// do nothing
 	}
-);
+	);
 
 // Submenu clicks
-$('.element-left ul.topnav > li').each(function() {
+$('.toolbar ul.topmenu > li').each(function() {
 	var button = $(this);
-	var menu = button.find('ul.subnav');
-	$(this).find('ul.subnav > li > a').click(function() {
+	var menu = button.find('ul.submenu');
+	if (!menu.length) {
+		return;
+	}
+	$(this).find('ul.submenu > li > a').click(function() {
 		menu.fadeOut(fadeTime);
 		button.removeClass('open');
 		lastOpen = null;
@@ -82,13 +230,19 @@ $('.element-left ul.topnav > li').each(function() {
 		}
 	});
 });
-	
+
+// Submenu hover 
+$('.toolbar ul.submenu a').hover(function() {
+		$(this).parent().addClass('open');
+	}, function() {
+		$(this).parent().removeClass('open');
+	});
 
 // Submenu closes on page click
-$('.everything:not(.nav)').click(function() {
+$('.everything:not(.toolbar)').click(function() {
 	if (lastOpen) {
 		// if a menu is open and the click is not on a new menu
-		closeMenu(lastOpen.find('ul.subnav'));
+		closeMenu(lastOpen.find('ul.submenu'));
 		lastOpen.removeClass('open');
 		lastOpen = null;
 	}
@@ -117,97 +271,3 @@ $(function() {
 		}
 	})
 });
-
-
-var fadeTime = 10;
-function openMenu(menu) {
-	menu.fadeIn(fadeTime);
-	menu.css('top', $('.nav table').height());
-	if (menu.offset().left + menu.width() > $(window).width()) {
-		menu.css('left', $(window).width()- (menu.offset().left + menu.width()));
-	}
-}
-
-function closeMenu(menu) {
-	menu.fadeOut(fadeTime);
-}
-
-function startTime() {
-	var today=new Date();
-	var month=today.getMonth();
-	var date=today.getDate();
-	var h=today.getHours();
-	var m=today.getMinutes();
-	var amPm = getAmPm(h);
-	switch(month) {
-		case 0:
-			month = 'January';
-			break;
-		case 1:
-			month = 'February';
-			break;
-		case 2:
-			month = 'March';
-			break;
-		case 3:
-			month = 'April';
-			break;
-		case 4:
-			month = 'May';
-			break;
-		case 5:
-			month = 'June';
-			break;
-		case 6:
-			month = 'July';
-			break;
-		case 7:
-			month = 'August';
-			break;
-		case 6:
-			month = 'September';
-			break;
-		case 6:
-			month = 'October';
-			break;
-		case 6:
-			month = 'November';
-			break;
-		case 6:
-			month = 'December';
-			break;
-		default:
-			month = '';
-			break;
-
-	}
-	h = fixHours(h);
-	m = fixMinutes(m);
-	document.getElementById('clock').innerHTML = month + ' ' + date + ', ' + h + ':' + m + ' ' + amPm;
-	var t = setTimeout(function(){
-		startTime()
-	},1000);
-}
-
-function getAmPm(h) {
-	if (h >= 12) {
-		return 'PM';
-	} else {
-		return 'AM';
-	}
-}
-
-function fixHours(h) {
-	if (h > 12) {
-		return h - 12;
-	} else if (h == 0) {
-		return 12;
-	} else {
-		return h;
-	}
-}
-
-function fixMinutes(i) {
-    if (i<10) {i = '0' + i};  // add zero in front of numbers < 10
-    return i;
-}
