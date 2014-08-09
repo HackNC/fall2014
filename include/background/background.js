740cbdb57b27250f5dcb9ef68c2722bd867e9d60
Vance Miller <vbmiller@cs.unc.edu>
2014-08-08 23:20:22 -0400

    loading animation

--- include/background/background.js
+++ include/background/background.js
@@ -1,35 +1,23 @@
-function setBackground(image) {
-	var size = '320';
-	var width = $(window).width();
-	if (width <= 640) {
-		size = '640';
-	} else if (width <= 800) {
-		size = '800';
-	} else if (width <= 1024) {
-		size = '1024';
-	} else if (width <= 1600) {
-		size = '1600';
-	} else {
-		size = '2048';
-	}
-	$('.background').css({'background-image': 'url(./images/backgrounds/' + image + '_' + size + '.jpg)'});
-}
+// function setBackground(image) {
+// 	var size = '320';
+// 	var width = $(window).width();
+// 	if (width <= 640) {
+// 		size = '640';
+// 	} else if (width <= 800) {
+// 		size = '800';
+// 	} else if (width <= 1024) {
+// 		size = '1024';
+// 	} else if (width <= 1600) {
+// 		size = '1600';
+// 	} else {
+// 		size = '2048';
+// 	}
+// 	$('.background').css({'background-image': 'url(./images/backgrounds/' + image + '_' + size + '.jpg)'});
+// }
 
-function selectRandomBackground() {
-	var numImages = 11;
-	return Math.floor(Math.random() * (numImages + 1));
-}
+// function selectRandomBackground() {
+// 	var numImages = 11;
+// 	return Math.floor(Math.random() * (numImages + 1));
+// }
 
-var response;
-$.ajax({ type: "GET",   
-     url: "include/background/background.html",   
-     async: false,
-     success : function(text)
-     {
-         response= text;
-     }
-});
-$('body').append(response).queue(function(next) {
-	setBackground(selectRandomBackground());
-	next();
-});
+// setBackground(selectRandomBackground());
--- include/splash/splash.js
+++ include/splash/splash.js
@@ -1,38 +1,60 @@
 // create a mask over the header with the splash.css style
-var response;
-$.ajax({ type: "GET",   
-     url: "include/splash/splash.html",   
-     async: false,
-     success : function(text)
-     {
-         response= text;
-     }
-});
-$('body').append(response).queue(function(next) {
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		// mobile
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	// mobile
+	setLogoSize();
+	setBackground(selectRandomBackground());
+	$('.splash > .container').css('display', 'table-cell');
+	$('.splash').delay(1000).fadeOut(500);
+} else {
+	// everything else
+	setLogoSize();
+	window.addEventListener('resize', function(event) {
 		setLogoSize();
-		$('.splash > .container').css('display', 'table-cell');
-		$('.splash').delay(1000).fadeOut(500);
-	} else {
-		// everything else
-		setLogoSize();
-		window.addEventListener('resize', function(event) {
-			setLogoSize();
+	});
+	$('.splash > .container').css('display', 'table-cell');
+	$('.splash > .container').addClass('zoomInEntrance').delay(10).queue(function(next) {
+		var image = getBackgroundString(selectRandomBackground()), img = $('<img />');
+		img.bind('load', function() {
+			$('.splash > .container').delay(1000).queue(function(next) {
+				$(this).removeClass('zoomInEntrance');
+				$(this).delay(500).addClass('zoomInExit').delay(400).parent().fadeOut();
+				next();
+			});
 		});
-		$('.splash > .container').css('display', 'table-cell');
-		$('.splash > .container').addClass('zoomInEntrance').delay(1900).queue(function(next){
-			$(this).removeClass('zoomInEntrance');
-			$(this).addClass('zoomInExit').delay(400).parent().fadeOut();
-			next();
-		});
-	}
-	next();
-});
+		img.attr('src', image);
+		$('.background').css('background-image', 'url(' + image + ')');
+		next();
+	});
+
+
+
+}
 
 // loading sequence
 function setLogoSize() {
 	var height = $(window).height();
 	$('.splash .container').css('height', height);
 	$('.splash .container').css('max-height', height);
+}
+
+function getBackgroundString(image) {
+	var size = '320';
+	var width = $(window).width();
+	if (width <= 640) {
+		size = '640';
+	} else if (width <= 800) {
+		size = '800';
+	} else if (width <= 1024) {
+		size = '1024';
+	} else if (width <= 1600) {
+		size = '1600';
+	} else {
+		size = '2048';
+	}
+	return './images/backgrounds/' + image + '_' + size + '.jpg';
+}
+
+function selectRandomBackground() {
+	var numImages = 11;
+	return Math.floor(Math.random() * (numImages + 1));
 }
