1f6f893fa3ba9e10e0475f4782248b757291f28a
Vance Miller <vbmiller@cs.unc.edu>
2014-08-11 15:18:38 -0400

    Revert "does this fix scrolling?"
    This reverts commit 9861d4b3f7a219939c264925f64ab71a9aa2344e.

--- javascript/main.js
+++ javascript/main.js
@@ -19,48 +19,19 @@ if (navigator.userAgent.indexOf('Chrome') != -1) {
 
 ////////////////// function definitions //////////////////
 // scrolling on anchor clicks
-function filterPath(string) {
-  return string
-    .replace(/^\//,'')
-    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-    .replace(/\/$/,'');
-  }
-  var locationPath = filterPath(location.pathname);
-  var scrollElem = scrollableElement('html', 'body');
- 
-  $('a[href*=#]').each(function() {
-    var thisPath = filterPath(this.pathname) || locationPath;
-    if (  locationPath == thisPath
-    && (location.hostname == this.hostname || !this.hostname)
-    && this.hash.replace(/#/,'') ) {
-      var $target = $(this.hash), target = this.hash;
-      if (target) {
-        var targetOffset = $target.offset().top - $('.toolbar > table').height();
-        $(this).click(function(event) {
-          event.preventDefault();
-          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
-            location.hash = target;
-          });
-        });
-      }
-    }
-  });
- 
-  // use the first element that is "scrollable"
-  function scrollableElement(els) {
-    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
-      var el = arguments[i],
-          $scrollElement = $(el);
-      if ($scrollElement.scrollTop()> 0) {
-        return el;
-      } else {
-        $scrollElement.scrollTop(1);
-        var isScrollable = $scrollElement.scrollTop()> 0;
-        $scrollElement.scrollTop(0);
-        if (isScrollable) {
-          return el;
-        }
-      }
-    }
-    return [];
-  }
+$(function() {
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			$target = $(this.hash);
+			$target = $target.length ? $target : $('[name=' + this.hash.slice(1) +']');
+			if ($target.length) {
+				$('html,body').animate({
+			        // subtract toolbar height;
+			        scrollTop: ($target.offset().top-$('.toolbar > table').height())
+			    }, 500);
+				$target.fadeOut(100).fadeIn(500);
+				return false;
+			}
+		}
+	});
+});
