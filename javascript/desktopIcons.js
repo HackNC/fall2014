eaf98086dc838dd759ae3dd65518df9fde64459d
Vance Miller <vbmiller@cs.unc.edu>
2014-08-11 19:41:43 -0400

    more icons

--- javascript/desktopIcons.js
+++ javascript/desktopIcons.js
@@ -9,3 +9,52 @@ desktopIcons_setDesktopSize();
 window.addEventListener('resize', function(event) {
 	desktopIcons_setDesktopSize();
 });
+
+$('#pizzaLink').click(function() {
+	if ($('#pizza').is(':visible')) {
+		$('#pizza').hide();
+	} else {
+		$('#pizza').show();
+	}
+});
+
+$('#mentorsLink').click(function() {
+	if ($('#mentors').is(':visible')) {
+		$('#mentors').hide();
+	} else {
+		$('#mentors').show();
+	}
+});
+
+$('#scheduleLink').click(function() {
+	if ($('#schedule').is(':visible')) {
+		$('#schedule').hide();
+	} else {
+		$('#schedule').show();
+	}
+});
+
+$('#prizesLink').click(function() {
+	if ($('#prizes').is(':visible')) {
+		$('#prizes').hide();
+	} else {
+		$('#prizes').show();
+	}
+});
+
+$('#mapLink').click(function() {
+	if ($('#map').is(':visible')) {
+		$('#map').hide();
+	} else {
+		$('#map').show();
+	}
+});
+
+$('.popup').click(function(e) {
+	if (e.target === this) {
+		if ($(this).is(':visible')) {
+			$(this).hide();
+		}
+	}
+});
+
